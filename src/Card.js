22ca565102674cfe0ec69ffa40028b8c4482f61d
Maninder Singh Bawa <maninderbawavip@gmail.com>
2021-02-25 16:55:33 +0530

    made card changes

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 import './index.css';
 
-
 const Card=(props)=> {
   return (
-    <div className='Card' style={{ backgroundImage: props.imgg}}>
-    <p><img src={props.imgg} class="CelebImage"></img></p>
+    <div className='Card' style={{ backgroundImage: `url(${props.imgg})`, backgroundRepeat: "no-repeat", backgroundSize: 'contain'  }} >
+    <p></p>
       <p>Name: {props.name}</p>
       <p> Actress</p>
       <p> Mumbai </p>
